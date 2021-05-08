33e6e23157a7bc0152857a89de01875213471927
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-05-08 17:23:02 -0300

    fix: update UpdateUser composition

--- src/main/factories/usecases/db-update-user-factory.ts
+++ src/main/factories/usecases/db-update-user-factory.ts
@@ -2,4 +2,8 @@ import { DbUpdateUser } from '@/application/usecases';
 import { makeTypeormUserRepository } from '@/main/factories/db';
 
 export const makeDbUpdateUser = (): DbUpdateUser =>
-  new DbUpdateUser(makeTypeormUserRepository(), makeTypeormUserRepository());
+  new DbUpdateUser(
+    makeTypeormUserRepository(),
+    makeTypeormUserRepository(),
+    makeTypeormUserRepository(),
+  );
