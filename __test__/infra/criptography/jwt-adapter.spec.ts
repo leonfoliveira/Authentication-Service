8d6eb788493b10b93a29278a8e81e3883e8b37dc
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-05-08 12:27:15 -0300

    feat: make JwtAdapter implement StatelessTokenDecoder

--- __test__/infra/criptography/jwt-adapter.spec.ts
+++ __test__/infra/criptography/jwt-adapter.spec.ts
@@ -11,6 +11,12 @@ const makeSut = (secret = faker.random.word()): JwtAdapter => {
     .spyOn(jwt, 'sign')
     .mockClear()
     .mockImplementation((): string => faker.datatype.uuid());
+  jest
+    .spyOn(jwt, 'verify')
+    .mockClear()
+    .mockImplementation(
+      (): Record<string, any> => ({ [faker.database.column()]: faker.random.word() }),
+    );
   const sut = new JwtAdapter(secret);
 
   return sut;
@@ -36,7 +42,41 @@ describe('JwtAdapter', () => {
       const sut = makeSut();
 
       const result = sut.generate(mockDTO());
+
       expect(result).toBe(getReturn(jest.spyOn(jwt, 'sign')));
+    });
+  });
+
+  describe('StatelessTokenDecoder', () => {
+    it('should call jwt.verify with correct params', () => {
+      const secret = faker.random.word();
+      const sut = makeSut(secret);
+      const statelessToken = faker.datatype.uuid();
+
+      sut.decode(statelessToken);
+
+      expect(jwt.verify).toHaveBeenCalledWith(statelessToken, secret);
+    });
+
+    it('should return the same as jwt.verify', () => {
+      const sut = makeSut();
+
+      const result = sut.decode(faker.datatype.uuid());
+
+      expect(result).toBe(getReturn(jest.spyOn(jwt, 'verify')));
+    });
+
+    it('should return null if jwt.verify throws', () => {
+      const sut = makeSut();
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(
+        (): Record<string, any> => {
+          throw new Error();
+        },
+      );
+
+      const result = sut.decode(faker.datatype.uuid());
+
+      expect(result).toBe(null);
     });
   });
 });
--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -1,11 +1,19 @@
 import jwt from 'jsonwebtoken';
 
-import { StatelessTokenGenerator } from '@/application/interfaces';
+import { StatelessTokenDecoder, StatelessTokenGenerator } from '@/application/interfaces';
 
-export class JwtAdapter implements StatelessTokenGenerator {
+export class JwtAdapter implements StatelessTokenGenerator, StatelessTokenDecoder {
   constructor(private readonly secret: string) {}
 
   generate(payload: Record<string, any>): string {
     return jwt.sign(payload, this.secret);
+  }
+
+  decode(statelessToken: string): Record<string, any> {
+    try {
+      return jwt.verify(statelessToken, this.secret) as Record<string, any>;
+    } catch {
+      return null;
+    }
   }
 }
