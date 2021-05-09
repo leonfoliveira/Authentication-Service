62605782a02e8c42e11c8996bf85908f9c3585b5
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-05-08 22:40:47 -0300

    feat: add cookie-parser

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Express } from 'express';
 import helmet from 'helmet';
@@ -5,6 +6,7 @@ import helmet from 'helmet';
 export default (app: Express): void => {
   app.use(helmet());
   app.use(cors());
+  app.use(cookieParser());
   app.use(express.json());
   app.use(express.static('public'));
 };
