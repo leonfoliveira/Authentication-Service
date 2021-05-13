b0814301f454a37573e2edf4c32c79f7cdfd50c2
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-05-12 23:39:06 -0300

    test: add RefreshAuthorizationRoute integration test

--- __test__/infra/entities/index.ts
+++ __test__/infra/entities/index.ts
@@ -1,1 +1,2 @@
+export * from './mock-refresh-token';
 export * from './mock-user-entity';
---
+++ __test__/infra/entities/mock-refresh-token.ts
@@ -0,0 +1,12 @@
+import { RefreshTokensEntity, UserEntity } from '@/infra/entities';
+
+export const mockRefreshToken = async (
+  user: UserEntity,
+  token = 'any_token',
+): Promise<RefreshTokensEntity> => {
+  const refreshToken = new RefreshTokensEntity();
+  refreshToken.user = user;
+  refreshToken.token = token;
+  refreshToken.issuedAt = new Date();
+  return refreshToken.save();
+};
